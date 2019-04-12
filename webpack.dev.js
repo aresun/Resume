ac01797d9e6f4ba91a9489c52f8dd53754ff2f47
Aresun <2014229503@qq.com>
2019-04-12 17:48:40 +0800

    remove non-useful alanysis webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const path = require('path');
 const merge = require('webpack-merge');
 const comConfig = require('./webpack.com');
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 let devConfig = {
     mode: 'development',
