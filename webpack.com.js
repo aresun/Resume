63cb314ead1c8adebd4f2c36772c8fa139ba5e53
Aresun <2014229503@qq.com>
2019-04-12 15:34:30 +0800

    npm: html-webpack-plugin clean-webpack-plugin

--- webpack.com.js
+++ webpack.com.js
@@ -1,7 +1,4 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -65,15 +62,15 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'AICODER 全栈线下实习', // 默认值：Webpack App
+            title: 'Aresun resume', // 默认值：Webpack App
             filename: 'index.html', // 默认值： 'index.html'
-            template: path.resolve(__dirname, 'src/main.html'),
+            template: path.resolve(__dirname, 'src/index.html'),
             minify: {
                 collapseWhitespace: true,
                 removeComments: true, // 是否移除注释
                 removeAttributeQuotes: true // 移除属性的引号
             }
         }),
-        new CleanWebpackPlugin(['dist'])
+        new CleanWebpackPlugin()
     ]
 }
