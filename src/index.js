76f7f72743004daf5f499887612e61ad7891dfe0
Aresun <2014229503@qq.com>
2019-04-13 16:04:27 +0800

    finished [skills] section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { l } from './js/common';
+import { l, randomInt } from './js/common';
 import { briefIntro, skills } from './js/data';
 
 import './sass/page.scss';
@@ -10,6 +10,10 @@ import universityImg from './img/university.png';
 import diplomaImg from './img/diploma.png';
 
 const page = document.getElementById('page');
+const skillsTagsContainer = document.getElementById('skills').getElementsByTagName('ul')[0];
+const subscriptContainer = skillsTagsContainer.nextSibling;
+const maxSpeed = 3;
+
 const imgs = [
     emailImg,
     homeImg,
@@ -35,29 +39,123 @@ function addImgs() {
 }
 addImgs();
 
-function appendBriefs(b){
+function appendBriefs(b) {
     let target = document.getElementById('brief').getElementsByClassName('left')[0].getElementsByTagName('ul')[0];
     let dom;
     let frag = document.createDocumentFragment();
-    for(let brief of b){
+    for (let brief of b) {
         dom = document.createElement('li');
         dom.textContent = brief;
-        dom.insertBefore(document.createElement('div'),dom.childNodes[0]);
+        dom.insertBefore(document.createElement('div'), dom.childNodes[0]);
         frag.appendChild(dom);
     }
     target.appendChild(frag);
 }
 appendBriefs(briefIntro);
 
-function appendSkills(sks){
+function appendSkills(sks) {
     /* add default description(for s.description) */
-    let frag = document.createDocumentFragment();
-    for(let s of sks){
+    let fragForTags = document.createDocumentFragment();
+    let fragForDescri = document.createDocumentFragment();
+    for (let i in sks) {
         /* add all tags(for s.name) */
         let dom = document.createElement('span');
-        dom.textContent = s.name;
-        frag.appendChild(dom);
+        dom.textContent = sks[i].name;
+        dom.dataset.index = i;
+        fragForTags.appendChild(dom);
+
+        /* add all descriptions */
+        dom = document.createElement('span');
+        dom.textContent = sks[i].description;
+        dom.style.position = 'absolute';
+        dom.style.left = `${randomInt(0, 900)}px`;
+        dom.style.top = `${randomInt(0, 80)}px`;
+        dom.dataset.speed = randomInt(2, maxSpeed);
+        dom.dataset.index = i;
+        fragForDescri.appendChild(dom);
     }
-    document.getElementById('skills').getElementsByTagName('ul')[0].appendChild(frag);
+
+    skillsTagsContainer.appendChild(fragForTags);
+    subscriptContainer.appendChild(fragForDescri);
 }
 appendSkills(skills);
+function addSkillsTagHoverEvent() {
+    skillsTagsContainer.addEventListener('mouseover', (e) => {
+        let thisDom = e.target; // e.target
+        if (thisDom.nodeName === 'SPAN') {
+            // target's siblings
+            let targetDoms = subscriptContainer.getElementsByTagName('span');
+            // target's index
+            let targetIndex = parseInt(thisDom.dataset.index);
+            let len = targetDoms.length;
+            /* stop the target description */
+            // use index to mapping to the target
+            let targetDom = targetDoms[targetIndex];
+            targetDom.dataset.speed = 0;
+            /* set other spans' opacity */
+            for (let i = 0; i !== len; i++) {
+                if (i !== targetIndex) {
+                    targetDoms[i].style.opacity = 0.2;
+                }
+            }
+        }
+        e.stopPropagation();
+    });
+    skillsTagsContainer.addEventListener('mouseout', (e) => {
+        let thisDom = e.target;
+        if (thisDom.nodeName === 'SPAN') {
+            let targetDoms = subscriptContainer.getElementsByTagName('span');
+            let targetIndex = thisDom.dataset.index;
+            let len = targetDoms.length;
+            let targetDom = targetDoms[targetIndex];
+            // set target's speed
+            targetDom.dataset.speed = randomInt(2, maxSpeed);
+            /* recover opacity */
+            for (let i = 0; i !== len; i++) {
+                if(i !== targetIndex){
+                    targetDoms[i].style.opacity = 1;
+                }
+            }
+        }
+        e.stopPropagation();
+    });
+}
+addSkillsTagHoverEvent();
+/* -------------text carousel------------- */
+let requestAnimationFrame;
+let aniMove = (timestamp) => {
+    let descriArr = subscriptContainer.getElementsByTagName('span');
+    let len = descriArr.length;
+    let thisOne;
+    let x, y;
+    for (let i = 0; i !== len; i++) {
+        thisOne = descriArr[i];
+        [x, y] = [thisOne.style.left, thisOne.style.top];
+        x = parseInt(x);
+        if (x < 0) {
+            x = 900;
+            y = randomInt(0, 80);
+            thisOne.dataset.speed = randomInt(2, maxSpeed);
+        }
+        thisOne.style.left = `${x - thisOne.dataset.speed}px`;
+    }
+};
+function startAni() {
+    aniMove();
+    const aniID = requestAnimationFrame(startAni);
+}
+if (window.requestAnimationFrame) {
+    requestAnimationFrame = window.requestAnimationFrame;
+} else if (window.mozRequestAnimationFrame) {
+    requestAnimationFrame = window.mozRequestAnimationFrame;
+} else if (window.webkitRequestAnimationFrame) {
+    requestAnimationFrame = window.webkitRequestAnimationFrame;
+} else if (window.msRequestAnimationFrame) {
+    requestAnimationFrame = window.msRequestAnimationFrame;
+} else {
+    l('Sorry! your browser not support this animation');
+}
+if (requestAnimationFrame) {
+    startAni();
+}
+/* -------------text carousel end------------- */
--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,17 @@
-function l(...args){
-    console.log.apply(console,args);
+function l(...args) {
+    console.log.apply(console, args);
 }
-
-export {l};
+function randomInt(start, range) {
+    let result = start + Math.floor(Math.random() * (range + 1));
+    return result;
+}
+/*  */
+function getStyle(element, attr) {
+    if (element.currentStyle) {
+        return element.currentStyle[attr];
+    } else {
+        // 2rd arg: 伪元素字符串（例如“:after”）
+        return getComputedStyle(element, null)[attr];
+    }
+}
+export { l, randomInt, getStyle };
