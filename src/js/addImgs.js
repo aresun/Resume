4ffe9ea0529d85960b5917827bcb881fdf2f25eb
Aresun <2014229503@qq.com>
2019-04-24 21:45:04 +0800

    change images; add 2 fonts

--- src/js/addImgs.js
+++ src/js/addImgs.js
@@ -1,36 +1,39 @@
-import emailImg from '../img/email.png';
-import homeImg from '../img/home.png';
-import phoneImg from '../img/phone.png';
-import birthImg from '../img/birth.png';
-import universityImg from '../img/university.png';
-import diplomaImg from '../img/diploma.png';
+import emailImg from "../img/email.png";
+import homeImg from "../img/home.png";
+import phoneImg from "../img/phone.png";
+import birthImg from "../img/birth.png";
+import universityImg from "../img/university.png";
+import expertiseImg from "../img/expertise.png";
 
 const imgs = [
-    emailImg,
-    homeImg,
-    phoneImg,
-    birthImg,
-    universityImg,
-    diplomaImg
+  phoneImg,
+  emailImg,
+  birthImg,
+  homeImg,
+  universityImg,
+  expertiseImg
 ];
 /**
  * @param {dom} d dom, target
  * @param {img} i image
  */
 function appendPic(d, i) {
-    let dom = document.createElement('img');
-    dom.src = i;
-    d.appendChild(dom);
+  let dom = document.createElement("img");
+  dom.src = i;
+  d.appendChild(dom);
 }
 /**
  * @description add all images to li's back
  */
 function addImgs() {
-    let sources = document.getElementById('brief').getElementsByClassName('right')[0].getElementsByTagName('li');
-    let len = sources.length;
-    let i = 0;
-    for (; i !== len; i++) {
-        appendPic(sources[i], imgs[i]); // one li, one image
-    }
+  let sources = document
+    .getElementById("brief")
+    .getElementsByClassName("right")[0]
+    .getElementsByTagName("li");
+  let len = sources.length;
+  let i = 0;
+  for (; i !== len; i++) {
+    appendPic(sources[i], imgs[i]); // one li, one image
+  }
 }
 export default addImgs;
